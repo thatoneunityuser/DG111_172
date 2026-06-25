30047390ea60bb94c93d61eeeb65ee9587d77aaf
thatoneunityuser <130134083+thatoneunityuser@users.noreply.github.com>
2026-06-25 19:38:44 +0700

    Split printf statements for better readability

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main() 
 {
- printf("สวัสดีครับ ผม สิรธีร์ สุนทรภูติวงศ์ \n รหัสนักศึกษา: 692110172 \n วิชา: DG111 Computer Programming");
+  printf("สวัสดีครับ ผม สิรธีร์ สุนทรภูติวงศ์ \n ");
+  printf("รหัสนักศึกษา: 692110172 \n ");
+  printf("วิชา: DG111 Computer Programming");
 
  return 0;
 }
